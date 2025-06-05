28073ee909e14cbfaa2377194b3a70c42974778d
Nbashcontract <nnbush@icloud.com>
2025-06-06 02:36:35 +0300

    созданы все окна

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,53 @@ namespace Chisldiferen
         {
             InitializeComponent();
         }
+
+        private void LeftMehtod_Click(object sender, RoutedEventArgs e)
+        {
+            Left left= new Left();
+            left.Show();
+        }
+
+        private void QuadricMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Quadric quadric= new Quadric();
+            quadric.Show();
+        }
+
+        private void RungeMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Runge runge= new Runge();
+            runge.Show();
+        }
+
+        private void NutonMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Nuton nuton= new Nuton();
+            nuton.Show();
+        }
+
+        private void CentreMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Centre centre= new Centre();
+            centre.Show();
+        }
+
+        private void MnkMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Mnk mnk= new Mnk();
+            mnk.Show();
+        }
+
+        private void CubicMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Cubic cubic= new Cubic();
+            cubic.Show();
+        }
+
+        private void RightMethod_Click(object sender, RoutedEventArgs e)
+        {
+            Right right= new Right();
+            right.Show();
+        }
     }
 }
